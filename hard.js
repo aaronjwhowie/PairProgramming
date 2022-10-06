0b78de810d44eea5689eca3b2f3bc0a14e36bb80
aaronjwhowie <aaronjwhowie@gmail.com>
2022-10-05 23:39:19 -0400

    Submission 2

---
+++ hard.js
@@ -0,0 +1,11 @@
+// Hard (1)
+// Write a function that takes in a string and returns a boolean value whether or not the string contains a pair of matching brackets ({}, [], ()). These brackets must be nested appropriately in order to return a true value
+// Example 1:
+// Input: str = “{hello world}” brackets(srt)
+// Output: True
+
+// Example 2:
+// Input: str = “{hello world]” brackets(srt)
+// Output: False
+
+//INCOMPLETE
